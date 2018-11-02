ec7b7c94871aa72c48561d7743bc862357a6dd83
Sasha Kramer <sashakramer@tru-signal.com>
2018-11-02 11:51:48 -0500

    adding tree

---
+++ src/player.js
@@ -0,0 +1,90 @@
+var blessed = require('blessed');
+var contrib = require('blessed-contrib');
+
+// Create a screen object.
+var screen = blessed.screen({
+    smartCSR: true
+});
+
+// var terminal = blessed.terminal({
+//     parent: screen,
+//     cursor: 'block',
+//     cursorBlink: true,
+//     screenKeys: false,
+//     label: ' terminals ',
+//     left: 0,
+//     top: 0,
+//     width: '100%',
+//     height: '100%',
+//     border: 'line',
+//     style: {
+//         fg: 'default',
+//         bg: 'default',
+//         focus: {
+//             border: {
+//                 fg: 'green'
+//             }
+//         }
+//     }
+// });
+// Create a box perfectly centered horizontally and vertically.
+var box = blessed.box({
+    top: 0,
+    left: 0,
+    width: '50%',
+    height: '50%',
+    content: 'Hello {bold}world{/bold}!',
+    tags: true,
+    border: {
+        type: 'line'
+    },
+    style: {
+        fg: 'white',
+        bg: 'magenta',
+        border: {
+            fg: '#f0f0f0'
+        },
+        hover: {
+            bg: 'green'
+        }
+    }
+});
+
+// Append our box to the screen.
+// screen.append(box);
+
+var tree = contrib.tree({
+    fg: 'green',
+    left: 0,
+    top: '50%',
+    width: '50%',
+    height: '50%',
+})
+screen.append(tree);
+
+tree.on('select', function(node){
+    // capture selects
+})
+
+tree.setData({
+    extended: true,
+    children: {
+        'options': {
+            children: {
+                'option one': {},
+                'option two': {}
+            }
+        }
+    }
+})
+
+
+// Quit on Escape, q, or Control-C.
+screen.key(['escape', 'q'], function (ch, key) {
+    return process.exit(0);
+});
+
+// terminal.focus();
+tree.focus();
+
+screen.render();
